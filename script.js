2efb7bb78a3639911712755dfb1a3113497af3e2
Vianey Garcia <garcia_vianey01@student.smc.edu>
2021-02-01 20:56:51 -0800

    Animation on Scroll Added
    Added Animation on scroll library and implimented it into spot page

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    AOS.init({
+        // Global settings:
+        disable: 'phone',
+        duration: 900, // values from 0 to 3000, with step 50ms
+        easing: 'ease', // default easing for AOS animations
+     });
+    });
