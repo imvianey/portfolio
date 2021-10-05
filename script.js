b043c3670e97af4d3470186784a6a30745194fe9
Vianey Garcia <garcia_vianey01@student.smc.edu>
2021-10-04 21:08:11 -0700

    Portfolio Webflow

--- script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    AOS.init({
-        // Global settings:
-        disable: 'mobile',
-        duration: 900, // values from 0 to 3000, with step 50ms
-        easing: 'ease', // default easing for AOS animations
-     });
-    });
